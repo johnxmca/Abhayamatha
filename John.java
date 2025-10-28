15eddf371a39050f767826880217e55304de0452
johnxmca <52282192+johnxmca@users.noreply.github.com>
2025-10-29 00:02:56 +0530

    Update John.java

--- John.java
+++ John.java
@@ -1,5 +1,6 @@
 public class John {
     public static void main(String[] args) {
         System.out.println("Hello, World! Jxxx11111");
+        System.out.println("Hello, World! Jxxx22222");
     }
 }
